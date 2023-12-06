3eb50afa75be87d2e107b960aa438e00ab1786ca
Rakesh Ayyaswami <rakeshta@gmail.com>
2023-12-07 02:49:31 +1100

    initialized from template

---
+++ updateDefs.js
@@ -0,0 +1,21 @@
+const https = require('https');
+const fs = require('fs');
+
+const url =
+  'https://raw.githubusercontent.com/bitburner-official/bitburner-src/dev/src/ScriptEditor/NetscriptDefinitions.d.ts';
+const path = './NetscriptDefinitions.d.ts';
+
+https
+  .get(url, (res) => {
+    const file = fs.createWriteStream(path);
+
+    res.pipe(file);
+
+    file.on('finish', () => {
+      file.close;
+      console.log('Netscript Type Definitions Updated');
+    });
+  })
+  .on('error', (err) => {
+    console.log('Error: ', err.message);
+  });
