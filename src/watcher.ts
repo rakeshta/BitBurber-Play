eddc68c67bfcbe61ffa6355cd38cceda1519d435
Rakesh Ayyaswami <rakeshta@gmail.com>
2023-12-08 03:13:07 +1100

    improved watcher using new logging utility

--- src/watcher.ts
+++ src/watcher.ts
@@ -1,44 +1,55 @@
 import { NS } from '@ns';
+import { init } from '/scripts/lib/util';
 
+/** Utility to restart a running script when it's updated. */
 export async function main(ns: NS): Promise<void> {
-  const hashes: Record<string, number> = {};
+  init(ns);
 
+  // collect hashes for all scripts in the home computer
+  const hashes: Record<string, number> = {};
   const files = ns.ls('home', '.js');
   for (const file of files) {
     const contents = ns.read(file);
-    hashes[file] = getHash(contents);
+    hashes[file] = computeHash(contents);
   }
 
+  // repeatedly check all scripts for updated & restart if necessary
   while (true) {
     const files = ns.ls('home', '.js');
-
     for (const file of files) {
+      // compute hash for the file. skip if it hasn't changed
       const contents = ns.read(file);
-      const hash = getHash(contents);
-
-      if (hash != hashes[file]) {
-        ns.tprintf(`INFO: Detected change in ${file}`);
-
-        const processes = ns.ps().filter((p) => p.filename == file);
-        for (const process of processes) {
-          ns.tprintf(`INFO: Restarting ${process.filename} ${process.args} -t ${process.threads}`);
-          if (process.filename != ns.getScriptName()) {
-            ns.kill(process.pid);
-            ns.run(process.filename, process.threads, ...process.args);
-          } else {
-            ns.spawn(process.filename, process.threads, ...process.args);
-          }
-        }
-
-        hashes[file] = hash;
+      const hash = computeHash(contents);
+      if (hash === hashes[file]) {
+        continue;
       }
+
+      // the script has changed
+      term.debug(`Detected change in ${file}`);
+
+      // find running processes for the script & restart them
+      const processes = ns.ps().filter((p) => p.filename == file);
+      for (const process of processes) {
+        term.info(`Restarting ${process.filename} ${process.args} -t ${process.threads}`);
+        if (process.filename != ns.getScriptName()) {
+          ns.kill(process.pid);
+          ns.run(process.filename, process.threads, ...process.args);
+        } else {
+          ns.spawn(process.filename, process.threads, ...process.args);
+        }
+      }
+
+      // save the updated hash
+      hashes[file] = hash;
     }
 
+    // wait a second and check again
     await ns.sleep(1000);
   }
 }
 
-const getHash = (input: string): number => {
+/** Computes a hash for the given string. */
+function computeHash(input: string): number {
   let hash = 0,
     i,
     chr;
@@ -49,4 +60,4 @@ const getHash = (input: string): number => {
     hash |= 0; // Convert to 32bit integer
   }
   return hash;
-};
+}
