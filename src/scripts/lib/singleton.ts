354cfcdc659fe9b141e1ceca669462de0f61bced
Rakesh Ayyaswami <rakeshta@gmail.com>
2023-12-10 03:32:28 +1100

    utility to ensure single instance of a script

---
+++ src/scripts/lib/singleton.ts
@@ -0,0 +1,24 @@
+/** Utilities to manage singleton processes. */
+export const singleton = {
+  /** Takes over as the new singleton instance by killing previous instances. */
+  replace(): void {
+    // iterate through running scripts and kill other instances
+    let isNotified = false;
+    const scriptName = ns.getScriptName();
+    for (const instance of ns.ps()) {
+      // abort if this is not an instance of this script
+      if (instance.filename !== scriptName || instance.pid === ns.pid) {
+        continue;
+      }
+
+      // notify once
+      if (!isNotified) {
+        term.warn(`Replacing previous instance of ${scriptName}`);
+        isNotified = true;
+      }
+
+      // kill previous instance
+      ns.kill(instance.pid);
+    }
+  },
+} as const;
